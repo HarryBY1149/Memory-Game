cfd6f86b232e4b6f31b5d14cc18000724478239b
H. B. Young <HarryBeckerYoung@gmail.com>
2018-12-19 18:19:42 -0500

    Updates

---
+++ precache-manifest.eddaf9aea72db10d5baeefc7aba7279c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aa354a30b0dd1708cc1",
+    "url": "/Memory-Game/static/js/runtime~main.1aa354a3.js"
+  },
+  {
+    "revision": "eba47a69b60e4f779c5d",
+    "url": "/Memory-Game/static/js/main.eba47a69.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/Memory-Game/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "eba47a69b60e4f779c5d",
+    "url": "/Memory-Game/static/css/main.f8ebb02c.chunk.css"
+  },
+  {
+    "revision": "e0e4c8706a4a6e31e9e00b261f0b2027",
+    "url": "/Memory-Game/index.html"
+  }
+];
